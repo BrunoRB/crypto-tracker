22f5899c25c203c35500e7ad21c541712d037a7b
BrunoRB <brunorb8@hotmail.com>
2018-07-21 19:53:56 -0300

    final touches

--- src/modules/confs.js
+++ src/modules/confs.js
@@ -12,31 +12,35 @@ let CONFS = {
 
 let PROD_CONFS = {};
 
-try {
-	PROD_CONFS = require('./../prod-confs.json');
-	debug('Found production confs', Object.keys(PROD_CONFS));
-}
-catch(e) {
-	debug('Production confs not found at ./../prod-confs.json');
-}
+const isNotVue = typeof __webpack_require__ !== 'function' && typeof process === 'object';
 
-if (!PROD_CONFS.MONEEDA_TOKEN && !process.env.MONEEDA_TOKEN) {
-	throw `
-		Missing MONEEDA_TOKEN.
-		Please include it in nodejs/src/prod-confs.json or use the ENV var "MONEEDA_TOKEN".
-	`;
-}
-// ENV always has precedence
-else if (process.env.MONEEDA_TOKEN) {
-	PROD_CONFS.MONEEDA_TOKEN = process.env.MONEEDA_TOKEN;
-}
+if (isNotVue) {
+	try {
+		PROD_CONFS = require('./../prod-confs.json');
+		debug('Found production confs', Object.keys(PROD_CONFS));
+	}
+	catch(e) {
+		debug('Production confs not found at ./../prod-confs.json');
+	}
 
-let token = PROD_CONFS.MONEEDA_TOKEN;
+	if (!PROD_CONFS.MONEEDA_TOKEN && !process.env.MONEEDA_TOKEN) {
+		throw `
+			Missing MONEEDA_TOKEN.
+			Please include it in src/prod-confs.json or use the ENV var "MONEEDA_TOKEN".
+		`;
+	}
+	// ENV always has precedence
+	else if (process.env.MONEEDA_TOKEN) {
+		PROD_CONFS.MONEEDA_TOKEN = process.env.MONEEDA_TOKEN;
+	}
 
-debug(
-	'MONEEDA_TOKEN',
-	`${token.substring(0, 4)}...${token.substring(token.length - 4)}`
-);
+	let token = PROD_CONFS.MONEEDA_TOKEN;
+
+	debug(
+		'MONEEDA_TOKEN',
+		`${token.substring(0, 4)}...${token.substring(token.length - 4)}`
+	);
+}
 
 for (let k in PROD_CONFS) {
 	CONFS[k] = PROD_CONFS[k];
